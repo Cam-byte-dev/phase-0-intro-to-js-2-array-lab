6abfa3a7f7d871db591449f379c415a4b4853133
Camron <cawe219@gmail.com>
2022-12-02 23:40:42 -0500

    commit tree 39738ca1ab53acb59288a4a68c55220a4505ac88

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
-// Write your solution here!
+
+      const cats = ["Milo","Otis","Garfield"];
+
+// function BeforeEach(){
+
+//     cats.length = 0;
+//     cats.push("Milo","Otis","Garfield");
+    
+//     } 
+
+
+function destructivelyAppendCat(name){
+
+    
+    name.push("Ralph");
+    return name;
+}
+
+function destructivelyPrependCat(name){
+   
+
+    name.unshift("Bob");
+    return name;
+    
+}
+
+
+console.log(destructivelyPrependCat(cats));
+
+ 
